c11ac394d53b2d3c2dce3eb1ac45bad99eef8fc0
Gavin Stark <gavin@gstark.com>
2021-01-16 21:01:40 -0500

    Update

---
+++ README.md
@@ -0,0 +1,30 @@
+# neocast-bootstrap
+
+> NEOCAST Bootstrap
+
+[![NPM](https://img.shields.io/npm/v/neocast-bootstrap.svg)](https://www.npmjs.com/package/neocast-bootstrap) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Install
+
+```bash
+npm install --save neocast-bootstrap
+```
+
+## Usage
+
+```jsx
+import React, { Component } from 'react'
+
+import MyComponent from 'neocast-bootstrap'
+import 'neocast-bootstrap/dist/index.css'
+
+class Example extends Component {
+  render() {
+    return <MyComponent />
+  }
+}
+```
+
+## License
+
+MIT © [gstark](https://github.com/gstark)
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Modal as _Modal } from './components/Bootstrap/Modal'
+import { Button as _Button } from './components/Bootstrap/Button'
+import { Collapse as _Collapse } from './components/Bootstrap/Collapse'
+import { Dropdown as _Dropdown } from './components/Bootstrap/Dropdown'
+import { DropdownMenu as _DropdownMenu } from './components/Bootstrap/DropdownMenu'
+import { DropdownToggle as _DropdownToggle } from './components/Bootstrap/DropdownToggle'
+import { UncontrolledDropdown as _UncontrolledDropdown } from './components/Bootstrap/UncontrolledDropdown'
+import { UncontrolledTooltip as _UncontrolledTooltip } from './components/Bootstrap/UncontrolledTooltip'
+
+export const Modal = _Modal
+export const Button = _Button
+export const Collapse = _Collapse
+export const Dropdown = _Dropdown
+export const DropdownMenu = _DropdownMenu
+export const DropdownToggle = _DropdownToggle
+export const UncontrolledDropdown = _UncontrolledDropdown
+export const UncontrolledTooltip = _UncontrolledTooltip
