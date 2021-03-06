be04a3a1d45234814cc22011badb282240f2469c
Gavin Stark <gavin@gstark.com>
2021-03-06 06:46:41 -0500

    Adds DropdownItem

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Modal as _Modal } from './components/Bootstrap/Modal'
 import { Button as _Button } from './components/Bootstrap/Button'
 import { Collapse as _Collapse } from './components/Bootstrap/Collapse'
 import { Dropdown as _Dropdown } from './components/Bootstrap/Dropdown'
+import { DropdownItem as _DropdownItem } from './components/Bootstrap/DropdownItem'
 import { DropdownMenu as _DropdownMenu } from './components/Bootstrap/DropdownMenu'
 import { DropdownToggle as _DropdownToggle } from './components/Bootstrap/DropdownToggle'
 import { UncontrolledDropdown as _UncontrolledDropdown } from './components/Bootstrap/UncontrolledDropdown'
@@ -14,6 +15,7 @@ export const Modal = _Modal
 export const Button = _Button
 export const Collapse = _Collapse
 export const Dropdown = _Dropdown
+export const DropdownItem = _DropdownItem
 export const DropdownMenu = _DropdownMenu
 export const DropdownToggle = _DropdownToggle
 export const UncontrolledDropdown = _UncontrolledDropdown
